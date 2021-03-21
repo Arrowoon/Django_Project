3392572d6aecfbeb2e24337c049127693696e066
Arrowoon <arrowoonbijay@gmail.com>
2021-03-21 19:14:39 +0545

    first commit

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,12 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', home, name='home'),
+    path('about', about, name='about'),
+    path('services', services, name='services'),
+    path('blog-home', bloghome, name='blog-home'),
+    path('price', price, name='price'),
+    path('contact', contact, name='contact'),
+    path('blog-single', blogsingle, name='blog-single'),
+    path('elements', elements, name='elements'),
+    path('portfolio', portfolio, name='portfolio'),
 ]
